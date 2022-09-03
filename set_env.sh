95c4f5aed9de45ba30ee43ebbf524da166f65c98
anobah <anobahe@yahoo.com>
2022-09-03 13:47:51 +0000

    First commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=hi027505028
+export POSTGRES_HOST=ebenezercdr.ciwcgxynljhf.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=ebendb
+export AWS_BUCKET=arn:aws:s3:::my-437227026509-bucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
